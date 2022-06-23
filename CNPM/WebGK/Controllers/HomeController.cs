cea11b09ed385639ce972a8e7fdaa327196905d6
PhuongTrang-62 <phuongtrang622002@gmail.com>
2022-06-23 08:26:39 +0700

    source moi

---
+++ CNPM/WebGK/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebGK.Models;
+namespace WebGK.Controllers
+{
+    public class HomeController : Controller
+    {
+        WebTHIGKEntities db = new WebTHIGKEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var Product = db.Products1.ToList();
+            ViewBag.Product = Product;
+            var Category = db.Categories.ToList();
+            ViewBag.Category = Category;
+            return View();
+        }
+    }
+}
